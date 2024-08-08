f23d5897263ec63260e9566859a8f392c03b1b37
Valéria <vvqf2016@gmail.com>
2024-08-07 23:38:02 -0300

    chore added cors settings

--- publish-subscribe/notification_system/settings.py
+++ publish-subscribe/notification_system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 # Asynchronous Server Gateway Interface,
@@ -56,6 +57,7 @@ CHANNEL_LAYERS = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'notification_system.urls'
 
