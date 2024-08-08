baf9243812c8bdd4823fb7a866a5e75c75a5f1a6
Valéria <vvqf2016@gmail.com>
2024-08-08 15:41:35 -0300

    chore return topic name instead of id

--- publish-subscribe/core/serializers.py
+++ publish-subscribe/core/serializers.py
@@ -3,10 +3,13 @@ from .models import NotificationTopic, UserSubscription, Notification
 
 
 class UserSubscriptionSerializer(serializers.ModelSerializer):
+    topic_name = serializers.SerializerMethodField()
     class Meta:
         model = UserSubscription
-        fields = ['id', 'user', 'topic']
+        fields = ['id', 'user', 'topic_name']
 
+    def get_topic_name(self, obj):
+        return obj.topic.name if obj.topic else None
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
